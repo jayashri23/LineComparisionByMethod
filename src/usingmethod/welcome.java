1cf6e0581b72a55d90f0b3e853bea4088433345e
jayashri23 <jsjankar23@gmail.com>
2022-10-20 04:02:15 +0530

    Welcome To Line Computation Program

---
+++ src/usingmethod/welcome.java
@@ -0,0 +1,7 @@
+package usingmethod;
+
+public class welcome {
+    public static void main(String [] args){
+        System.out.println("Welcome To Line Comparison Computation  Program ");
+    }
+}
