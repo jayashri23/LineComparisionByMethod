0d40b1ab2d5076ecfafd52d7e8406e8dada6569b
jayashri23 <jsjankar23@gmail.com>
2022-10-20 05:01:23 +0530

    Comparing Two Lines

--- src/usingmethod/LengthOfLine.java
+++ src/usingmethod/LengthOfLine.java
@@ -1,2 +1,27 @@
-package usingmethod;public class LengthOfLine {
+package usingmethod;
+
+import java.util.Scanner;
+
+public class LengthOfLine {
+
+    static void length(){
+
+        int x1, y1, x2, y2;
+        double length;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("x2= ");
+        x2 = sc.nextInt();
+        System.out.println("x1= ");
+        x1 = sc.nextInt();
+        System.out.println("y2= ");
+        y2 = sc.nextInt();
+        System.out.println("y1= ");
+        y1 = sc.nextInt();
+        length = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+        System.out.println("Length of Line ="  +length);
+    }
+    public static void main(String [] args){
+        System.out.println("Enter values for length calculation:" );
+        length();
+    }
 }
